c9019853cecc18a73c4cc37c2dbb91c2507888d8
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2025-04-29 13:58:15 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/mxmoder/Ankita-bot-movie-.git /Ankita-bot-movie- 
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Ankita-bot-movie- 
+fi
+cd /Ankita-bot-movie- 
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
