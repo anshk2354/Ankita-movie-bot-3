a084b4a0c8ab3ad3b5acc68a9c1fffc2507d224a
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2025-05-03 12:25:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
-
+MELCOW_VID = -1002655668331  # yeh ek Telegram channel/group ID ho sakta hai
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
