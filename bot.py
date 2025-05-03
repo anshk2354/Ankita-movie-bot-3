bf8d995ab854d11f1f13a85a2411ce8d229a1d99
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2025-05-03 13:04:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ async def Lazy_start():
     temp.B_NAME = me.first_name
     LazyPrincessBot.username = '@' + me.username
     logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-    logging.info(LOG_STR)
+    LOG_STR = "Lazy Bot started successfully!"
     logging.info(script.LOGO)
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
