56ebdf6f9fa3252d20043999c19f270e82e8e474
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2025-05-05 09:40:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 P_TTI_SHOW_OFF = True  # Ya aap isko False ya koi aur value set kar sakte hain
 SINGLE_BUTTON = False  # Ya True, jo aapko required ho
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://anshk2354:<simransh>@cluster0.fxqprvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://anshk2354:simransh@cluster0.fxqprvk.mongodb.net/?retryWrites=true&w=majority")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
