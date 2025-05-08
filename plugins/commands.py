8504f0dfe2c806fc55fb2390ea5e66a8d4a2d893
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2025-05-08 12:20:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -70,10 +70,10 @@ await message.reply_photo(
     caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
     reply_markup=reply_markup,
     parse_mode=enums.ParseMode.HTML
-)
-        return
+ )
+ return
     
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
+if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
